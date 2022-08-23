c3da5bcddf3182e66a5eb1760244f228471a811c
chen <chenleinxd@gmail.com>
2022-08-23 21:14:21 +0800

    fix: 本地锁工厂实现修改

--- ddd-infrastructure/src/main/java/com/chen/ddd/infrastructure/lock/ReentrantLockFactory.java
+++ ddd-infrastructure/src/main/java/com/chen/ddd/infrastructure/lock/ReentrantLockFactory.java
@@ -1,6 +1,8 @@
 package com.chen.ddd.infrastructure.lock;
 
 import com.chen.ddd.core.common.lock.LockFactory;
+import org.springframework.integration.support.locks.DefaultLockRegistry;
+import org.springframework.integration.support.locks.LockRegistry;
 
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
@@ -13,10 +15,11 @@ import java.util.concurrent.locks.ReentrantLock;
 // @Named
 public class ReentrantLockFactory implements LockFactory {
 
+    private final LockRegistry lockRegistry = new DefaultLockRegistry(1023);
 
     @Override
     public Lock create(String lockKey) {
-        return new ReentrantLock();
+        return lockRegistry.obtain(lockKey);
     }
 
 }
